71d0dcd2e7864f1034fd1a9123d0d0efcb4f3e09
Kabbilan <kabbilan5608@gmail.com>
2026-08-12 23:27:12 +0530

    Wrap AppShell in LanguageProvider for context

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,5 +1,14 @@
 import { AppShell } from '@/components/shell/app-shell'
+import { LanguageProvider } from '@/components/language-provider'
 
-export default function AppLayout({ children }: { children: React.ReactNode }) {
-  return <AppShell>{children}</AppShell>
+export default function AppLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <LanguageProvider>
+      <AppShell>{children}</AppShell>
+    </LanguageProvider>
+  )
 }
